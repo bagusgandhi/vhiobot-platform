281d6310b1a9e14e42ed4c5becbe5d4eb49f1cbf
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:22:59 +0700

    feat: add env utils

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,12 +1,12 @@
 import getConfig from 'next/config';
 
-const { serverRuntimeConfig } = getConfig();
+// const { serverRuntimeConfig } = getConfig();
 
 export const Env = {
-    GOOGLE_CLIENT_ID: serverRuntimeConfig.GOOGLE_CLIENT_ID as string,
-    GOOGLE_SECRET: serverRuntimeConfig.GOOGLE_SECRET as string,
-    SOCKET_URL: serverRuntimeConfig.SOCKET_URL as string,
-    VHIOBOT_API_HOST: serverRuntimeConfig.VHIOBOT_API_HOST as string,
-    NEXTAUTH_SECRET: serverRuntimeConfig.NEXTAUTH_SECRET as string,
-    SECRET_API_KEY: serverRuntimeConfig.SECRET_API_KEY as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET as string,
+    SOCKET_URL: process.env.SOCKET_URL as string,
+    VHIOBOT_API_HOST: process.env.VHIOBOT_API_HOST as string,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
+    SECRET_API_KEY: process.env.SECRET_API_KEY as string,
 }
