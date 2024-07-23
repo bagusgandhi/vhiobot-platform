6973a45f008a9c9d125f94065c301b5409d2c6cd
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:14:06 +0700

    feat: add role checking for accesing page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,41 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+
+import { type NextRequest, NextResponse } from 'next/server';
 
 export default withAuth({
   pages: {
-    signIn: '/',
+    signIn: '/admin-login',
+    error: undefined
+  },
+});
+
+export async function middleware(req: NextRequest) {
+  const token: any = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+
+  const isAdminPage = req.nextUrl?.pathname.startsWith('/admin');
+
+
+  if (!token) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.rewrite(url);
   }
-})
+
+  if (isAdminPage && token?.account?.response?.role !== 'ADMINISTRATOR') {
+    const url = req.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.rewrite(url);
+  }
+
+  // User has access, allow the request to proceed
+  return NextResponse.next();
+}
 
 export const config = {
-    matcher: ['/chat']
+  matcher: ['/admin/:path*', '/chat'],
 };
