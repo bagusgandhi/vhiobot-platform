01d04f1bc709b32285d8687d71fb8c2141fbf702
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:12:58 +0700

    feat: add restrictMode to false

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverRuntimeConfig: {
+    reactStrictMode: false,
+    env: {
         APP_ENV: process.env.APP_ENV,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_SECRET: process.env.GOOGLE_SECRET,
