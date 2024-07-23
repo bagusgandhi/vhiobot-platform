4d9971eaf0edb7944245113cdd0ed507447a657a
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-07-23 14:12:33 +0700

    feat: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        "primary": "#4942E4"
+      },
+      textColor: {
+        "primary": "#4942E4"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
