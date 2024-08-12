c0023ad097faef19d6ee043c8788ca571e129bbe
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-12 09:36:44 +0700

    feat: console log in middleware when erro

--- src/app/api/intent/route.ts
+++ src/app/api/intent/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: NextRequest) {
 
     return Response.json({data});
   } catch (error) {
-    // console.log(error)
+    console.log(error)
     return Response.json(
       { error: 'Failed to process the request' },
       { status: 500 },
